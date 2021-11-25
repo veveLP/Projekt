65778bfc07378540df27ed1003bf799c1146e042
veveLP <lukasekhouzar@seznam.cz>
2021-11-25 11:02:53 +0100

    ukol1

--- Projekt/Ukol1.cs
+++ Projekt/Ukol1.cs
@@ -16,5 +16,46 @@ namespace Projekt
         {
             InitializeComponent();
         }
+
+        private void Ukol1_Load(object sender, EventArgs e)
+        {
+            string pismena = "QERTYUIOPASDFGHJKLZXCVBNM.,?!";
+            int i = 0;
+            foreach (Control ctrl in panel1.Controls)
+            {
+                if (ctrl is Button) { (ctrl as Button).Text = pismena[i].ToString(); i++; }
+                
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ((Button)sender).Text;
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            string nwtext = "";
+            int numb = 3;
+            string text = textBox1.Text;
+            text = text.ToLower();
+            char[] znaky = text.ToCharArray();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (znaky[i].ToString() == " " || znaky[i].ToString() == "." || znaky[i].ToString() == "," || znaky[i].ToString() == "!" || znaky[i].ToString() == "?") { }
+                else
+                {
+                    int temp = (int)znaky[i];
+                    temp += numb;
+                    if (temp > 122) { temp -= 26; }
+                    znaky[i] = (char)temp;
+                }
+                nwtext += znaky[i].ToString();
+
+            }
+            textBox1.Text = nwtext.ToUpper();
+        }
     }
 }
