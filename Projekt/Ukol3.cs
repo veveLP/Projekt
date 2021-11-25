0304dbf28440b64cd0ea60318b695379fe08683e
veveLP <lukasekhouzar@seznam.cz>
2021-11-25 10:19:06 +0100

    Přidejte soubory projektu.

---
+++ Projekt/Ukol3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt
+{
+    public partial class Ukol3 : Form
+    {
+        public Ukol3()
+        {
+            InitializeComponent();
+        }
+    }
+}
