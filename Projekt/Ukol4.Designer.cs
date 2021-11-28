e3a3acc3af1622b078a8f74fc03347b34590186c
Filip Iran <iranf.03@spst.cz>
2021-11-28 14:33:58 +0100

    4.3

--- Projekt/Ukol4.Designer.cs
+++ Projekt/Ukol4.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.vypocitat = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // vypocitat
+            // 
+            this.vypocitat.Location = new System.Drawing.Point(12, 36);
+            this.vypocitat.Name = "vypocitat";
+            this.vypocitat.Size = new System.Drawing.Size(75, 23);
+            this.vypocitat.TabIndex = 5;
+            this.vypocitat.Text = "Vypočítat";
+            this.vypocitat.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(140, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(114, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "11";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "26";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "mocnina";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // Ukol4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(265, 124);
+            this.Controls.Add(this.vypocitat);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Ukol4";
             this.Text = "Ukol4";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button vypocitat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Projekt/Ukol4.cs
+++ Projekt/Ukol4.cs
@@ -16,5 +16,24 @@ namespace Projekt
         {
             InitializeComponent();
         }
+
+        private void vypocitat_Click(object sender, EventArgs e)
+        {
+            label1.Text = comboBox1.Text;
+
+            double mocnina = Convert.ToDouble(comboBox1.Text);
+
+            double cislo1 = Convert.ToDouble(textBox1.Text);
+            double cislo2 = Convert.ToDouble(textBox2.Text);
+            double cislo3 = Convert.ToDouble(textBox3.Text);
+
+            cislo1 = Math.Pow(cislo1, mocnina);
+            cislo2 = Math.Pow(cislo2, mocnina);
+            cislo3 = Math.Pow(cislo3, mocnina);
+
+            textBox1.Text = Convert.ToString(cislo1);
+            textBox2.Text = Convert.ToString(cislo2);
+            textBox3.Text = Convert.ToString(cislo3);
+        }
     }
 }
